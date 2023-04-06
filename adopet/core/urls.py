4ba08e470ad63a4bb9508d7ff462509a711c471f
Henrique Andrade <henrique.ccda@gmail.com>
2023-04-06 18:27:09 -0300

    Mudando o nome da views.

--- adopet/core/urls.py
+++ adopet/core/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "core"
 urlpatterns = [
     #
-    path("tutores/", views.TutorLC.as_view(), name="list-create-tutor"),
-    path("tutores/<int:pk>/", views.TutorRDU.as_view(), name="read-delete-update-tutor"),
+    path("tutores/", views.tutor_list_create, name="list-create-tutor"),
+    path("tutores/<int:pk>/", views.tutor_read_delete_update, name="read-delete-update-tutor"),
     #
     path("abrigos/", views.shelter_list_create, name="list-create-shelter"),
     path("abrigos/<int:pk>/", views.shelter_read_delete_update, name="read-delete-update-shelter"),
