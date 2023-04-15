0f081f36ff61cab9c905ee504d7de9a81b111633
Henrique Andrade <henrique.ccda@gmail.com>
2023-04-14 23:49:55 -0300

    Rota para deletar uma adoção. #54
    close #54

--- adopet/pet/tests/adoption/test_create.py
+++ adopet/pet/tests/adoption/test_create.py
@@ -10,10 +10,10 @@ pytestmark = pytest.mark.django_db
 URL = "pet:create-adoption"
 
 
-def test_positive(client_api, create_adoption_payload):
+def test_positive(client_api_auth_user, create_adoption_payload):
     url = resolve_url(URL)
 
-    resp = client_api.post(url, data=create_adoption_payload)
+    resp = client_api_auth_user.post(url, data=create_adoption_payload)
     assert resp.status_code == status.HTTP_201_CREATED
 
     adoption = Adoption.objects.first()
@@ -31,6 +31,76 @@ def test_positive(client_api, create_adoption_payload):
     assert resp["Location"] == f"http://testserver/adoption/{adoption.pk}/"
 
 
+def test_negative_pet_must_have_active_equal_to_true(client_api_auth_user, create_adoption_payload, pet):
+    data = create_adoption_payload.copy()
+
+    url = resolve_url(URL)
+
+    pet.is_active = False
+    pet.save()
+
+    data["pet"] = pet.pk
+
+    resp = client_api_auth_user.post(url, data=data)
+    assert resp.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_negative_pet_must_have_adopted_equal_to_false(client_api_auth_user, create_adoption_payload, pet):
+    data = create_adoption_payload.copy()
+
+    url = resolve_url(URL)
+
+    pet.is_adopted = True
+    pet.save()
+
+    data["pet"] = pet.pk
+
+    resp = client_api_auth_user.post(url, data=data)
+    assert resp.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_negative_tutor_must_have_active_equal_to_true(client_api_auth_user, create_adoption_payload, tutor):
+    data = create_adoption_payload.copy()
+
+    url = resolve_url(URL)
+
+    tutor.is_active = False
+    tutor.save()
+
+    data["tutor"] = tutor.pk
+
+    resp = client_api_auth_user.post(url, data=data)
+    assert resp.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_negative_tutor_must_have_is_tutor_equal_to_true(client_api_auth_user, create_adoption_payload, tutor):
+    data = create_adoption_payload.copy()
+
+    url = resolve_url(URL)
+
+    tutor.is_tutor = False
+    tutor.save()
+
+    data["tutor"] = tutor.pk
+
+    resp = client_api_auth_user.post(url, data=data)
+    assert resp.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_negative_tutor_must_have_is_shelter_equal_to_false(client_api_auth_user, create_adoption_payload, tutor):
+    data = create_adoption_payload.copy()
+
+    url = resolve_url(URL)
+
+    tutor.is_shelter = True
+    tutor.save()
+
+    data["tutor"] = tutor.pk
+
+    resp = client_api_auth_user.post(url, data=data)
+    assert resp.status_code == status.HTTP_400_BAD_REQUEST
+
+
 @pytest.mark.parametrize(
     "field",
     [
@@ -39,14 +109,14 @@ def test_positive(client_api, create_adoption_payload):
         "date",
     ],
 )
-def test_negative_missing_fields(client_api, field, create_adoption_payload):
+def test_negative_missing_fields(client_api_auth_user, field, create_adoption_payload):
     data = create_adoption_payload.copy()
 
     del data[field]
 
     url = resolve_url(URL)
 
-    resp = client_api.post(url, data=data)
+    resp = client_api_auth_user.post(url, data=data)
 
     assert resp.status_code == status.HTTP_400_BAD_REQUEST
 
@@ -66,14 +136,14 @@ def test_negative_missing_fields(client_api, field, create_adoption_payload):
         ("date", -1, "Formato inválido para data. Use um dos formatos a seguir: YYYY-MM-DD."),
     ],
 )
-def test_negative_validation_errors(client_api, field, value, error, create_adoption_payload):
+def test_negative_validation_errors(client_api_auth_user, field, value, error, create_adoption_payload):
     data = create_adoption_payload.copy()
 
     data[field] = value
 
     url = resolve_url(URL)
 
-    resp = client_api.post(url, data=data)
+    resp = client_api_auth_user.post(url, data=data)
 
     assert resp.status_code == status.HTTP_400_BAD_REQUEST
 
@@ -81,3 +151,15 @@ def test_negative_validation_errors(client_api, field, value, error, create_adop
     body = resp.json()
 
     assert body[field] == [error]
+
+
+def test_negative_must_be_auth(client_api):
+    url = resolve_url(URL)
+
+    resp = client_api.delete(url)
+
+    assert resp.status_code == status.HTTP_401_UNAUTHORIZED
+
+    body = resp.json()
+
+    assert body["detail"] == "As credenciais de autenticação não foram fornecidas."
